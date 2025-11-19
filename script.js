3e41e524ed6d9d9461ca75ad732cc680b1f84eab
Meval Purohit <mevalpurohit2828@gmail.com>
2025-11-19 22:14:44 +0530

    sixth dom

--- script.js
+++ script.js
@@ -1,38 +1,31 @@
 let main = document.querySelector("main");
 let btn = document.querySelector("button");
+let inner = document.querySelector(".inner")
+let h1 = document.querySelector(".percent")
+let change = document.querySelector(".change h1")
 
-let arr = ["https://plus.unsplash.com/premium_photo-1760468898862-ab3172853206?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXxQMmI0V2c3eldxc3x8ZW58MHx8fHx8","https://plus.unsplash.com/premium_photo-1760466136484-f95907bdfdfb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MnxQMmI0V2c3eldxc3x8ZW58MHx8fHx8","https://plus.unsplash.com/premium_photo-1760466051119-f47dd9edaafa?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NXxQMmI0V2c3eldxc3x8ZW58MHx8fHx8","https://plus.unsplash.com/premium_photo-1760466136683-5ab1a7a1c7bf?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTV8UDJiNFdnN3pXcXN8fGVufDB8fHx8fA%3D%3D","https://images.unsplash.com/photo-1762966160841-37423cb6c242?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0fHx8ZW58MHx8fHx8","https://images.unsplash.com/photo-1762325658409-5d8aa0e43261?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3fHx8ZW58MHx8fHx8","https://images.unsplash.com/photo-1688610683988-f71245b518bd?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE3fHx8ZW58MHx8fHx8"]
 
-
+let grow =0
 
 btn.addEventListener("click",function(){
-  let div= document.createElement("div")
-   let x = Math.random()*95
-  let y = Math.random()*80
-  // let c1 = Math.floor(Math.random()*256)
-  // let c2 = Math.floor(Math.random()*256)
-  // let c3 = Math.floor(Math.random()*256)
-  let r= Math.floor(Math.random()*360)
-  let bgimg = arr[Math.floor(Math.random()*arr.length)]
-  // let scale= Math.floor(Math.random()*3)
+  btn.style.pointerEvents='none'
+  let num = 20 + Math.floor(Math.random()*50)
+  console.log(`your dowload will complete in `+ num +` seconds`)
 
- 
+  let time = setInterval(function(){
+    grow++
+    h1.innerHTML=grow+"%"
+    inner.style.width=grow + "%"
+  },num)
 
-  div.style.width="200px"
-  div.style.height="300px"
-  console.log(bgimg)
-  div.style.backgroundImage= "url("+ bgimg + ")"
-  div.style.backgroundSize= "contain"
-  div.style.backgroundRepeat= "no-repeat"
-
-  div.style.position="absolute"
-  div.style.left= x +"%"
-  div.style.top= y +"%"
-  div.style.rotate= r+"deg"
-
- 
-
-  main.appendChild(div)
+  setTimeout(function(){
+    clearInterval(time)
+    btn.style.backgroundColor="#8b8b8b"
+    change.innerHTML="Downloaded"
+    change.style.color="#d1d0d0"
+  },num*100)
 })
+
+
 
 
