5edc2a2e4955540eaec3ba4c7c45ce6359ce365b
Meval Purohit <mevalpurohit2828@gmail.com>
2025-11-20 16:39:51 +0530

    insta-2 like

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 let main = document.querySelector("main");
 
-let box = document.querySelector("img")
+
+let box = document.querySelector(".box .love img")
 let love = document.querySelector('.box i')
+let like = document.querySelector(".com .like .liking")
 
 box.addEventListener("dblclick",function(){
     love.style.opacity="1"
-    love.style. transform = "translate(-50%,-50%) scale(1) rotate(0deg)"
+    love.style.transform = "translate(-50%,-50%) scale(1) rotate(0deg)"
+    like.style.transform="scale(1.4)"
+    like.style.color="red"
 
     setTimeout(function(){
-      love.style. transform = "translate(-50%,-300%) scale(1) rotate(60deg)"
+      love.style.transform = "translate(-50%,-300%) scale(1) rotate(60deg)"
+      like.style.transform="scale(1)"
     },700)
 
     setTimeout(function(){
