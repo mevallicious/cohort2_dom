f4e20d804c19d304aec462706526ccd60b64f543
Meval Purohit <mevalpurohit2828@gmail.com>
2025-11-20 14:59:26 +0530

    insta like

--- script.js
+++ script.js
@@ -1,31 +1,24 @@
 let main = document.querySelector("main");
-let btn = document.querySelector("button");
-let inner = document.querySelector(".inner")
-let h1 = document.querySelector(".percent")
-let change = document.querySelector(".change h1")
+
+let box = document.querySelector("img")
+let love = document.querySelector('.box i')
+
+box.addEventListener("dblclick",function(){
+    love.style.opacity="1"
+    love.style. transform = "translate(-50%,-50%) scale(1) rotate(0deg)"
+
+    setTimeout(function(){
+      love.style. transform = "translate(-50%,-300%) scale(1) rotate(60deg)"
+    },700)
+
+    setTimeout(function(){
+        love.style.opacity="0"
+    },1000)
+
+    setTimeout(function(){
+      love.style. transform = "translate(-50%,-50%) scale(0) rotate(-60deg)"
+    },1200)
 
 
-let grow =0
-
-btn.addEventListener("click",function(){
-  btn.style.pointerEvents='none'
-  let num = 20 + Math.floor(Math.random()*50)
-  console.log(`your dowload will complete in `+ num +` seconds`)
-
-  let time = setInterval(function(){
-    grow++
-    h1.innerHTML=grow+"%"
-    inner.style.width=grow + "%"
-  },num)
-
-  setTimeout(function(){
-    clearInterval(time)
-    btn.style.backgroundColor="#8b8b8b"
-    change.innerHTML="Downloaded"
-    change.style.color="#d1d0d0"
-  },num*100)
 })
-
-
-
 
